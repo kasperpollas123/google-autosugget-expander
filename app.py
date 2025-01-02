d9f3a2131d361f027876d8a8b92b8d97c97ab685
kasperpollas123 <worldofpollas@gmail.com>
2025-01-02 22:33:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def generate_expanded_keywords(seed_keyword):
 # Function to fetch keywords concurrently using multi-threading
 def fetch_keywords_concurrently(queries):
     all_keywords = set()
-    with ThreadPoolExecutor(max_workers=10) as executor:  # Adjust max_workers as needed
+    with ThreadPoolExecutor(max_workers=20) as executor:  # Increased max_workers to 20
         futures = {executor.submit(get_autosuggest, query): query for query in queries}
         for i, future in enumerate(as_completed(futures), start=1):
             try:
